9397a6df8e51725c2e644d2b010be794f646e401
sepideh asgharpoor hooshiar <sepideh.a.hooshiar@gmail.com>
2026-01-01 18:18:11 +0330

    avocados project is created and some image assets are added.

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by sepideh aph on 2026-01-01.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
